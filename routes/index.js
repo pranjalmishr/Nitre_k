3582f31ab1f379618b64d1f0c2ed324fbfedee36
pranjalmishr <mishrapranjal232@gmail.com>
2022-06-07 12:48:54 +0530

    get

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req)
-  res.json({name:"pranjal"})
-  // res.render('index', { title: 'Express' });
+  res.render('index')
 });
 
 module.exports = router;
