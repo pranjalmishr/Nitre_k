26a52e26e1230c613db6aff6c38947f84428dfbc
pranjalmishr <mishrapranjal232@gmail.com>
2022-06-07 14:32:27 +0530

    login

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var studentRouter = require('./routes/student');
 var teacherRouter = require('./routes/teacher');
+var loginRouter = require('./routes/login');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/student', studentRouter);
 app.use('/teacher', teacherRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
