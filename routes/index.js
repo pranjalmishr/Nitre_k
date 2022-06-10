bfa94fea238c3f6f81de5a36d91db7ff8322c485
pranjalmishr <mishrapranjal232@gmail.com>
2022-06-10 16:18:07 +0530

    insert

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-/* GET home page. */
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "pranjal"
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index')
+    con.connect(function(err) {
+        if (err) console.log("err");
+        console.log("Connected to mySQL!");
+      });
+    res.render('index')
+});
+
+router.post('/about', function(req, res, next) {
+    console.log(req.body)
+    var sql = "INSERT INTO `qqq` (`fname`, `lname`) \
+    VALUES ('"+req.body.fname+"', '"+req.body.lname+"');"
+    console.log(sql)
+   con.connect()
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+      });
+      var sql1 = "select * from `qqq`"
+      con.query(sql1, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+res.json({"Name":req.body})
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,62 +1,15 @@
 var express = require('express');
 var router = express.Router();
-// var modelLogin = require("../model/login")
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "192.168.43.1",
-  user: "root",
-  password: "1234",
-  database: "app_db"
-});
-
-
-
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-    console.log("Ayush");
-    con.connect(function(err) {
-        console.log("Abhinav")
-        if (err) console.log("err");
-        console.log("Connected!");
-      });
-    // modelLogin.a(modelLogin.pandy.name)
-console.log("Pranjal")
-    res.render('login')
-});
 
-router.get('/getLogin', function(req, res, next) {
-    console.log(req.query)
-    // res.send({"name":"himani"})
-    //res.render('teacher')
-res.json({"Name":requ})
+  console.log(req)
+  res.json({name:"pranjal"})
 });
-router.post('/getLogin', function(req, res, next) {
-    console.log(req.body)
-    // res.send({"name":"himani"})
-    //res.render('teacher')
-    var sql = "INSERT INTO `login` (`fname`, `lname`) \
-    VALUES ('"+req.body.fname+"', '"+req.body.lname+"');"
-    console.log(sql)
-   con.connect()
-      con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log(result);
-
-      });
-res.json({"Name":req.body})
+// });
+router.get('/:da-:ta', function(req, res, next) {
+      console.log(req.params)
+      res.json({name:req.params.da+","+req.params.ta+"="+req.params.da+req.params.ta})
 });
-
-router.all('/getPost', function(req, res, next) {
-    console.log(req.body)
-    if(req.body){
-        //do something
-    }else{
-        //do something else
-    }
-    // res.send({"name":"himani"})
-    //res.render('teacher')
-    console.log(req.body.pass)
-res.json({"Name":req.body})
-});
-
 module.exports = router;
