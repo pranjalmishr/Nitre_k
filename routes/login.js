7131c9c2259b9c73926643783d0f54a644a10e51
pranjalmishr <mishrapranjal232@gmail.com>
2022-06-10 16:33:37 +0530

    insert

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-/* GET users listing. */
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "data"
+});
+
 router.get('/', function(req, res, next) {
+    con.connect(function(err) {
+        if (err) console.log("err");
+        console.log("Connected to mySQL!");
+      });
+    res.render('login')
+});
 
-  console.log(req)
-  res.json({name:"pranjal"})
+router.post('/about', function(req, res, next) {
+    console.log(req.body)
+    var sql = "INSERT INTO `form_details` (`email`, `pass`) \
+    VALUES ('"+req.body.email+"', '"+req.body.pass+"');"
+    console.log(sql)
+   con.connect()
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+      });
+      // var sql1 = "select * from `qqq`"
+      // con.query(sql1, function (err, result) {
+      //   if (err) throw err;
+      //   console.log(result);
+      // });
+res.json({"Name":req.body})
 });
-// });
-router.get('/:da-:ta', function(req, res, next) {
-      console.log(req.params)
-      res.json({name:req.params.da+","+req.params.ta+"="+req.params.da+req.params.ta})
-});
+
 module.exports = router;
